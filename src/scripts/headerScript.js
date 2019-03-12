f5fa66595f4d228e3ae73cc43001e6ede31a31e4
Filip Lipowiecki <lipowiecki.f@gmail.com>
2019-03-12 23:25:17 +0100

    onscroll animations

--- src/scripts/headerScript.js
+++ src/scripts/headerScript.js
@@ -1,7 +1,7 @@
 let nav = document.querySelector('nav');
 
 window.onscroll =function myFunction() {
-    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
         nav.className = "navAnimateIn";
     } else {
         nav.className = "navAnimateOut";
